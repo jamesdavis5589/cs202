3d1c91e04a69c4d4ac207a674d30c32312dfc798
James Davis <james.davis5589@outlook.com>
2020-01-19 14:13:51 -0900

    Added values and tests utilizing the values and current functions

--- HW0/Tempurature/main.cpp
+++ HW0/Tempurature/main.cpp
@@ -9,6 +9,12 @@ This program will convert between Celsius and Farenheit
 #include <stdlib.h>
 #include <sstream>
 using std::stringstream;
+#include <vector>
+using std::vector;
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
 
 //(0°C × 9/5) + 32 = 32°F
 
@@ -21,18 +27,29 @@ double cpp_ftoc(const char* str)
 	ss << str;
 	ss >> x;
 
-	return x-32/(9/5)
+	return x - 32 / (9 / 5);
 }
 
 //Celsius to Farenheit
 double c_ctof(const char* str)
 {
-	int x = strtod(str, nullptr);
+	double x = strtod(str, nullptr);
 
-	return x*(9/5)+32
+	return x * (9.0 / 5.0) + 32.0;
 }
 
 int main(int argc, char* argv[])
 {
+	vector<char> temps; //{'0', '10', '25', '50', '100'}; <-- Didn't work for some reason...
+	temps.push_back('0');
+	temps.push_back('10');
+	temps.push_back('25');
+	temps.push_back('50');
+	temps.push_back('100');
 
+	cout << "Converting " << temps[0] << " degrees farenheit into celsius:" << endl;
+	cout << cpp_ftoc(temps[0]) << endl;
+
+	cout << "Converting " << temps[0] << " degrees celsius into farenheit:" << endl;
+	cout << c_ctof(temps[0]) << endl;
 }

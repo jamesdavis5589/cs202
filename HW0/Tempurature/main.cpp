fb21217e9436fea994aea45b14a96c971b18b6af
James Davis <james.davis5589@outlook.com>
2020-01-19 13:43:20 -0900

    Added and defined cpp_ftoc and c_ctof functions

--- HW0/Tempurature/main.cpp
+++ HW0/Tempurature/main.cpp
@@ -3,8 +3,34 @@ James Davis
 January 19th, 2020
 CS202
 HW0 - Temperature - main.cpp
-This program will
+This program will convert between Celsius and Farenheit
 */
+
+#include <stdlib.h>
+#include <sstream>
+using std::stringstream;
+
+//(0°C × 9/5) + 32 = 32°F
+
+//Farenheit to Celsius
+double cpp_ftoc(const char* str)
+{
+	stringstream ss;
+	double x;
+
+	ss << str;
+	ss >> x;
+
+	return x-32/(9/5)
+}
+
+//Celsius to Farenheit
+double c_ctof(const char* str)
+{
+	int x = strtod(str, nullptr);
+
+	return x*(9/5)+32
+}
 
 int main(int argc, char* argv[])
 {

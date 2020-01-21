4c08b3951458e0836bf2e22e5c1032cfb487cb2e
James Davis <james.davis5589@outlook.com>
2020-01-21 10:51:50 -0900

    Started project

---
+++ Lab 1/lab1.cpp
@@ -0,0 +1,26 @@
+/*
+James Davis
+January 21st, 2020
+CS202
+Lab 1 - lab1.cpp
+This program will test out flow-of-control structures
+*/
+
+//THEME: Jumpsuits
+
+int main()
+{
+	//if / else
+
+	//switch
+
+	//function call
+
+	//while-loop
+
+	//do-while-loop
+
+	//for-loop
+
+	//range-based for-loop
+}

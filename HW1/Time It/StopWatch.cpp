7b73a9d4d4bec95129763c77fcd205900d35d0e7
James Davis <james.davis5589@outlook.com>
2020-01-25 12:40:21 -0900

    Started class Stopwatch

---
+++ HW1/Time It/StopWatch.cpp
@@ -0,0 +1,9 @@
+/*
+James Davis
+January 15th, 2020
+CS202
+HW1 - Time It - StopWatch.cpp
+This program will
+*/
+
+#include "StopWatch.h"
---
+++ HW1/Time It/StopWatch.h
@@ -0,0 +1,20 @@
+/*
+James Davis
+January 15th, 2020
+CS202
+HW1 - Time It - StopWatch.h
+This program will
+*/
+
+#ifndef StopWatch_h
+#define StopWatch_h
+
+class StopWatch
+{
+
+
+public:
+
+};
+
+#endif

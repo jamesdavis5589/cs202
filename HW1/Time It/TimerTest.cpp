18de17a46ff60569157d52eb7ae432dba9a0c58a
James Davis <james.davis5589@outlook.com>
2020-01-25 15:07:10 -0900

    filled in arrays with random int values

--- HW1/Time It/TimerTest.cpp
+++ HW1/Time It/TimerTest.cpp
@@ -12,9 +12,30 @@ This program test out class Stopwatch
 using std::cout;
 using std::cin;
 using std::endl;
+#include <random>
+using std::mt19937;
+using std::random_device;
+using std::uniform_int_distribution;
+
+void allocate(int* x, int s)
+{
+	//s = number of 0s
+
+	for (int i = 0; i < (10 ^ s); i++)
+	{
+		//Literal random stuff
+		random_device rand;
+		mt19937 ran2(rand);
+		uniform_int_distribution<int> dist(0, 100);
+		int ran3 = dist(ran2);
+
+		x[i] = ran3;
+	}
+}
 
 int main()
 {
+	//adding memory
 	int* a = new int[10];
 	int* b = new int[100];
 	int* c = new int[1000]; //1K
@@ -25,7 +46,18 @@ int main()
 	int* h = new int[100000000];
 	int* i = new int[1000000000]; //1B
 
+	//Filling arrays with random values
+	allocate(a, 1);
+	allocate(b, 2);
+	allocate(c, 3);
+	allocate(d, 4);
+	allocate(e, 5);
+	allocate(f, 6);
+	allocate(g, 7);
+	allocate(h, 8);
+	allocate(i, 9);
 
 
+	//removing memory
 	delete[] a, b, c, d, e, f, g, h, i;
 }

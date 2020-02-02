f28a0281ee66a05d516fe8603f0c04178afb45c2
James Davis <james.davis5589@outlook.com>
2020-02-02 09:25:19 -0900

    Added functions to class ValueC

--- HW2/LLWS/value.cpp
+++ HW2/LLWS/value.cpp
@@ -3,7 +3,31 @@ James Davis
 January 30th
 CS202
 HW2 - LLSR - value.cpp
-This program will
+This program will define the struct used for testing
 */
 
 #include "value.h"
+
+ValueC::ValueC() :
+	_data()
+{}
+
+int ValueC::pop_front()
+{
+
+}
+
+int ValueC::pop_front()
+{
+
+}
+
+void ValueC::push_front()
+{
+
+}
+
+void ValueC::push_back()
+{
+
+}
--- HW2/LLWS/value.h
+++ HW2/LLWS/value.h
@@ -3,18 +3,25 @@ James Davis
 January 30th
 CS202
 HW2 - LLSR - value.h
-This program will
+This program will be the prototype for the class used in main testing
 */
 
+#include <vector>
 
 #ifndef value_h
 #define value_h
 
-struct Value {
+//Value Collection
+struct ValueC {
+	ValueC();
 
+	int pop_front();
+	int pop_back();
+	void push_front();
+	void push_back();
 
 private:
-
+	std::vector<int> _data;
 };
 
 #endif

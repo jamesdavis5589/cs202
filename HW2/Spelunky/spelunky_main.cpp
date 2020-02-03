9c131bf7252b9954337f48559a3e44db47c53481
James Davis <james.davis5589@outlook.com>
2020-02-02 18:29:57 -0900

    Added a user input loop to main

--- HW2/Spelunky/spelunky_main.cpp
+++ HW2/Spelunky/spelunky_main.cpp
@@ -8,8 +8,68 @@ This program will run the main program using room and complex
 
 #include "complex.h"
 
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+void UI(Complex c)
+{
+	string inp;
+	int rm = 0;
+
+	while (inp != "stop")
+	{
+		cout << c.getSD(rm) << endl;
+		cout << c.getLD(rm) << endl;
+		cout << endl;
+
+		if (rm == 0)
+		{
+			cout << "Move forward : 1" << endl;
+			cin >> inp;
+
+			if (inp != "stop")
+			{
+				rm++;
+			}
+			else
+			{
+				break;
+			}
+		}
+		else if (rm == 14)
+		{
+			cout << endl;
+			break;
+		}
+		else
+		{
+			cout << "Move forward : 1" << endl;
+			cout << "Move back : 2" << endl;
+			cin >> inp;
+
+			if (inp == "1")
+			{
+				rm++;
+			}
+			else if (inp == "stop")
+			{
+				break;
+			}
+			else
+			{
+				rm--;
+			}
+		}
+
+	}
+}
+
 int main()
 {
 	Complex caves;
 	caves.read();
+
+	UI(caves);
 }

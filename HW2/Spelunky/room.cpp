74743c367c68cb06711d286bd82d061470803207
James Davis <james.davis5589@outlook.com>
2020-02-02 16:04:20 -0900

    Defined the room class, which holds the template for each room

--- HW2/Spelunky/room.cpp
+++ HW2/Spelunky/room.cpp
@@ -7,3 +7,29 @@ This program will
 */
 
 #include "room.h"
+
+Room::Room() :
+	_sDesc(),
+	_lDesc()
+{}
+
+Room::Room(string s1, string s2) :
+	_sDesc(s1),
+	_lDesc(s2)
+{}
+
+Room::~Room()
+{
+	_sDesc = "";
+	_lDesc = "";
+}
+
+void Room::setLD(string s)
+{
+	_lDesc = s;
+}
+
+void Room::setSD(string s)
+{
+	_sDesc = s;
+}
--- HW2/Spelunky/room.h
+++ HW2/Spelunky/room.h
@@ -6,14 +6,23 @@ HW2 - Spelunky - spelunkey_main.cpp
 This program will
 */
 
+#include <string>
+using std::string;
+
 #ifndef room_h
 #define room_h
 
-class room {
-
+class Room {
+	string _sDesc;
+	string _lDesc;
 
 public:
+	Room();
+	Room(string s1, string s2);
+	~Room();
 
+	void setLD(string s);
+	void setSD(string s);
 };
 
 #endif
